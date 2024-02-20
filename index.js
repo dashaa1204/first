ab92600489bb28eb658942b9bcd309b716cc6e9e
Дашноров дамдиндорж <23LP9512@G401-CC0213.local>
2024-02-20 12:40:01 +0800

    class

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const { products, users } = require("./dummy.json");
 const app = express();
+const fetch = require(`fetch`);
+const bodyParser = require(`body-parser`);
+app.use(express.json());
+var fs = require(`fs`);
 
 app.get("/products", (req, res) => {
   res.type = "application/json";
@@ -19,6 +23,32 @@ app.get("/usernames", (req, res) => {
   });
   console.log(usernames);
   res.send({ usernames });
+});
+
+app.post("/add-user", (req, res) => {
+  const addedUsers = req.body;
+
+  fs.readFile("dummy.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      const jsonFile = JSON.parse(data.toString());
+      console.log(jsonFile);
+      jsonFile.users.push(addedUsers);
+      fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+        if (err) {
+          console.log(err);
+          res.send("error happened");
+        } else {
+          console.log("success");
+          res.send("User added success");
+        }
+      });
+    }
+  });
+  res.status(200);
+  res.send("User added successfully");
 });
 
 app.listen(3001, () => {
